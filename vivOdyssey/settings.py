8c39e3041e2c88ae7670ef3e9034618725b28c27
Jennifer Black <JenniLBlack@outlook.com>
2021-01-02 21:22:34 +0000

    visual commit

--- vivOdyssey/settings.py
+++ vivOdyssey/settings.py
@@ -20,7 +20,17 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,12 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    'D:\\VivO\\vivOdyssey\\assets',
-    ]
+##STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+##STATIC_URL = '/static/'
+##STATICFILES_DIRS = [
+##    BASE_DIR / 'static',
+##    'D:\\VivO\\vivOdyssey\\assets',
+##    ]
 #add login redirect
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/' # new
