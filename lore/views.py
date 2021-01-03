cc3546bcaa703aca43cbc08650d1c50a1b974863
Jennifer Black <JenniLBlack@outlook.com>
2021-01-03 16:41:18 +0000

    secure b2 support

--- lore/views.py
+++ lore/views.py
@@ -10,7 +10,7 @@ class LoreView(TemplateView):
 
     def readLore(request):
         textFile = "text/BackStory.html"
-        textFilePath = os.path.join(settings.STATIC_ROOT, textFile)
+        textFilePath = os.path.join(settings.STATIC_URL, textFile)
         with open(textFilePath, 'r') as f:
             loreText = f.read()
         context = {'loreContents' : loreText }
