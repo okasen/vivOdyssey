79bfbdc59932297dacb108bc4ee532cd72945e47
Jennifer Black <JenniLBlack@outlook.com>
2021-01-03 13:46:46 +0000

    fix settings again

--- vivOdyssey/settings.py
+++ vivOdyssey/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/' # new
