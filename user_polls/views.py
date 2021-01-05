721030097da142ff036169ef73052a768371d578
Jennifer Black <JenniLBlack@outlook.com>
2021-01-05 14:20:18 +0000

    added polls

--- user_polls/views.py
+++ user_polls/views.py
@@ -4,10 +4,39 @@ from django.core import serializers
 from .forms import QuestionCreate
 from .models import Question
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import View
 
 # Create your views here.
 
-class QAView(generic.CreateView):
+def receiveQuestion(request):
+    if request.is_ajax and request.method == "GET":
+        question_title = request.GET.get("title", None)
+        if Question.objects.filter(title = title).exists(): #if the question with this title exists, invalid
+            return JsonResponse({"valid":False}, status = 200)
+        else:
+            return JsonResponse({"valid":True}, status = 200)
 
+    return JsonResponse({}, status = 400)
+
+
+class QAView(View):
+    form_class = QuestionCreate
     template_name = 'user_polls/polls.html'
+    def questionView(self, *args, **kwargs):
+        form = self.form_class()
+        questions = Question.objects.all()
+        return render(request, template_name, {"form": form, "questions": questions})
+
+    def postQuestion(self, *args, **kwargs):
+        if request.is_ajax and request.method == "POST":
+            question = QuestionCreate(self.request.POST)
+            if form.is_valid():
+                instance = form.save()
+                ser_instance = serializers.serialize('json', [ instance, ])
+                return JsonResponse({"instance": ser_instance}, status=200)
+            else:
+                return JsonResponse({"error message": form.errors}, status=400)
+
+        return JsonResponse({"error": ""}, status=400)
+
+
