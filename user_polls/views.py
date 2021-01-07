5195c8653acba95bdfef8290b9c8b6a5b44ece0a
Jennifer Black <JenniLBlack@outlook.com>
2021-01-07 23:15:27 +0000

    added polls

--- user_polls/views.py
+++ user_polls/views.py
@@ -49,7 +49,7 @@ class QAView(View):
                 qClass = self.model_class()
                 qid = self.request.POST.get('delId', None)
                 getId = get_object_or_404(Question, title = qid)
-                qClass.objects.filter(title=getId).delete()
+                Question.objects.filter(title=getId).delete()
                 instance = qClass.save()
                 ser_instance = serializers.serialize('json', [ instance, ])
                 return JsonResponse(ser_instance, status=200)
