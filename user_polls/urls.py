acc119ad7016d204750f4fea758dbc0700d5c8e9
Jennifer Black <JenniLBlack@outlook.com>
2021-01-07 17:59:13 +0000

    added polls

--- user_polls/urls.py
+++ user_polls/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import QAView
-from QAView import receiveQuestion
 
 urlpatterns = [
     path('polls/', QAView.as_view(), name="polls"),
-    path('get/ajax/validate/question', QAView.receiveQuestion, name = "receive_Question"),
 ]
