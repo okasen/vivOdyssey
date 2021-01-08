d9f045aacf7615f1c81a539a4b4fbb1da8cd3f71
Jennifer Black <JenniLBlack@outlook.com>
2021-01-08 00:00:19 +0000

    added polls

--- user_polls/views.py
+++ user_polls/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.views import View
 
 # import the logging library
-from django.settings import logging
+import logging
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
