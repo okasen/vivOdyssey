45f9799ccb52ceae34bfdde95401023872d2b31d
Jennifer Black <JenniLBlack@outlook.com>
2021-01-15 10:53:10 +0000

    security and fishing pond added

--- fishing/urls.py
+++ fishing/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import GameView
 
 urlpatterns = [
-	path('fishing/', GameView.as_view(), name='play fishing'),
+	path('fishing/', GameView.as_view(), name='playfishing'),
 ]
