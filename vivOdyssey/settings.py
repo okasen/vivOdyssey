6730052ad917a2e3ea1ffbd70a829494c2bdffff
Jennifer Black <JenniLBlack@outlook.com>
2021-05-23 00:12:05 +0100

    redesigning CSS, adding navigation bar

--- vivOdyssey/settings.py
+++ vivOdyssey/settings.py
@@ -165,8 +165,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vivodyssey',
-        'USER': 'okasen',
-        'PASSWORD': os.getenv('PG_PASS'),
+        'USER': 'postgres',#'okasen',
+        'PASSWORD': 'postgres',#os.getenv('PG_PASS'),
         'OPTIONS': {
             'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
         },
